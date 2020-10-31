a95c7d20c9c753d812537da6ee4af02b633ec9b9
Alejandro Aranda <alexwing@gmail.com>
2020-10-31 20:51:36 +0100

    Change tools to accordeon

--- src/components/ToolsPanel.js
+++ src/components/ToolsPanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 import { GithubPicker } from 'react-color';
 
 
@@ -13,38 +14,38 @@ class ToolsPanel extends Component {
         {c}
       </option>
     ));
-    // Styles for continent selector
-    const selectStyles = {
-      position: 'absolute',
-      zIndex: 1
-    };
-    const { lineWidth, continent, onChangelineWidth, onChangeColor, onChangeColorStroke, onChangeContinent } = this.props;
-    return <Card>
-      <Card.Header>{this.props.name}</Card.Header>
-      <Card.Body>
-        <Card.Title>Modify the values to apply the changes to the map .</Card.Title>
+    const {name, lineWidth,continent, onChangelineWidth, onChangeColor, onChangeColorStroke,onChangeContinent } = this.props;
+    return <Accordion defaultActiveKey="0">
+    <Card>
+    <Accordion.Toggle as={Card.Header} eventKey="0">
+      {name}
+    </Accordion.Toggle>
+      <Accordion.Collapse eventKey="0">
+        <Card.Body>
         <Form>
-          <Form.Group controlId="formSelect">
-            <Form.Label>Continent: </Form.Label>
-            <Form.Control as="select" onChange={onChangeContinent} value={continent}>
-              {options}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="formBasicRange">
-            <Form.Label>Stroke Size</Form.Label>
-            <Form.Control type="range" min="0" max="4" value={lineWidth} onChange={onChangelineWidth} />
-          </Form.Group>
-          <Form.Group controlId="formGithubPicker1">
-            <Form.Label>Stroke Color: </Form.Label>
-            <GithubPicker onChangeComplete={onChangeColorStroke} />
-          </Form.Group>
-          <Form.Group controlId="formGithubPicker2">
-            <Form.Label>Polygon Color: </Form.Label>
-            <GithubPicker onChangeComplete={onChangeColor} />
-          </Form.Group>
-        </Form>
-      </Card.Body>
-    </Card>;
+            <Form.Group controlId="formSelect">
+              <Form.Label>Continent: </Form.Label>
+              <Form.Control as="select" onChange={onChangeContinent} value={continent}>
+                {options}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="formBasicRange">
+              <Form.Label>Stroke Size</Form.Label>
+              <Form.Control type="range" min="0" max="4" value={lineWidth} onChange={onChangelineWidth} />
+            </Form.Group>
+            <Form.Group controlId="formGithubPicker1">
+              <Form.Label>Stroke Color: </Form.Label>
+              <GithubPicker onChangeComplete={onChangeColorStroke} />
+            </Form.Group>
+            <Form.Group controlId="formGithubPicker2">
+              <Form.Label>Polygon Color: </Form.Label>
+              <GithubPicker onChangeComplete={onChangeColor} />
+            </Form.Group>
+          </Form> 
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>    
+  </Accordion>
   }
 
 }
